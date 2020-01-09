ad1b488d9b0b1ade437309d689d173d9cb4454b2
Scott Keplinger <skeplin17@gmail.com>
2020-01-09 23:04:29 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -20,3 +20,8 @@ function ageChecker(age) {
   }
     return ("You are a grownup")
 }
+
+function ternaryTeenager (age) {
+  return age>=13 && age<=19 ? "You are a teenager!" : "You are not a teenager"
+  
+}
